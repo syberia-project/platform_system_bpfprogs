482dfa1ca63eb209866ff3a7b3aeb3daada7b4e1
Joel Fernandes <joelaf@google.com>
2018-11-27 18:09:19 -0800

    Add test programs for testing BPF loader
    Change-Id: I47f98184618df526cc9e5c452defaa1f9a73ef82
    Signed-off-by: Joel Fernandes <joelaf@google.com>

---
+++ test/bpf_load_tp_prog.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/bpf.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "../helpers.h"
+
+struct bpf_map_def SEC("maps") cpu_pid = {
+        .type = BPF_MAP_TYPE_ARRAY,
+        .key_size = sizeof(int),
+        .value_size = sizeof(uint32_t),
+        /* Assume max of 1024 CPUs */
+        .max_entries = 1024,
+};
+
+struct switch_args {
+    unsigned long long ignore;
+    char prev_comm[16];
+    int prev_pid;
+    int prev_prio;
+    long long prev_state;
+    char next_comm[16];
+    int next_pid;
+    int next_prio;
+};
+
+SEC("tracepoint/sched/sched_switch")
+int tp_sched_switch(struct switch_args* args) {
+    int key;
+    uint32_t val;
+
+    key = bpf_get_smp_processor_id();
+    val = args->next_pid;
+
+    bpf_map_update_elem(&cpu_pid, &key, &val, BPF_ANY);
+    return 0;
+}
+
+char _license[] SEC("license") = "GPL";
