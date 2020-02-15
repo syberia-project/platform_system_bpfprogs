12a2fcad49cc521a41ddbb726658bb8bdd8514f5
Connor O'Brien <connoro@google.com>
2020-02-14 21:09:29 -0800

    time_in_state: set group & modes for maps & progs
    Set all maps & programs to the system group and grant read and/or
    write access as appropriate so that system_server can use them.
    
    Test: boot, ls -l /sys/fs/bpf shows correct results
    Test: system_server successfully starts tracking & reads data
    Bug: 138317993
    Change-Id: I0670db069d332cd31c5d8819e2a502b82eab9559
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- time_in_state.c
+++ time_in_state.c
@@ -17,20 +17,20 @@
 #include <bpf_helpers.h>
 #include <bpf_timeinstate.h>
 
-DEFINE_BPF_MAP(uid_time_in_state_map, PERCPU_HASH, time_key_t, tis_val_t, 1024)
+DEFINE_BPF_MAP_GRW(uid_time_in_state_map, PERCPU_HASH, time_key_t, tis_val_t, 1024, AID_SYSTEM)
 
-DEFINE_BPF_MAP(uid_concurrent_times_map, PERCPU_HASH, time_key_t, concurrent_val_t, 1024)
-DEFINE_BPF_MAP(uid_last_update_map, HASH, uint32_t, uint64_t, 1024)
+DEFINE_BPF_MAP_GRW(uid_concurrent_times_map, PERCPU_HASH, time_key_t, concurrent_val_t, 1024, AID_SYSTEM)
+DEFINE_BPF_MAP_GRW(uid_last_update_map, HASH, uint32_t, uint64_t, 1024, AID_SYSTEM)
 
-DEFINE_BPF_MAP(cpu_last_update_map, PERCPU_ARRAY, uint32_t, uint64_t, 1)
+DEFINE_BPF_MAP_UGM(cpu_last_update_map, PERCPU_ARRAY, uint32_t, uint64_t, 1, AID_ROOT, AID_SYSTEM, 0620)
 
-DEFINE_BPF_MAP(cpu_policy_map, ARRAY, uint32_t, uint32_t, 1024)
-DEFINE_BPF_MAP(policy_freq_idx_map, ARRAY, uint32_t, uint8_t, 1024)
+DEFINE_BPF_MAP_UGM(cpu_policy_map, ARRAY, uint32_t, uint32_t, 1024, AID_ROOT, AID_SYSTEM, 0620)
+DEFINE_BPF_MAP_UGM(policy_freq_idx_map, ARRAY, uint32_t, uint8_t, 1024, AID_ROOT, AID_SYSTEM, 0620)
 
-DEFINE_BPF_MAP(freq_to_idx_map, HASH, freq_idx_key_t, uint8_t, 2048)
+DEFINE_BPF_MAP_UGM(freq_to_idx_map, HASH, freq_idx_key_t, uint8_t, 2048, AID_ROOT, AID_SYSTEM, 0620)
 
-DEFINE_BPF_MAP(nr_active_map, ARRAY, uint32_t, uint32_t, 1)
-DEFINE_BPF_MAP(policy_nr_active_map, ARRAY, uint32_t, uint32_t, 1024)
+DEFINE_BPF_MAP_UGM(nr_active_map, ARRAY, uint32_t, uint32_t, 1, AID_ROOT, AID_SYSTEM, 0620)
+DEFINE_BPF_MAP_UGM(policy_nr_active_map, ARRAY, uint32_t, uint32_t, 1024, AID_ROOT, AID_SYSTEM, 0620)
 
 struct switch_args {
     unsigned long long ignore;
@@ -43,8 +43,8 @@ struct switch_args {
     int next_prio;
 };
 
-SEC("tracepoint/sched/sched_switch")
-int tp_sched_switch(struct switch_args* args) {
+DEFINE_BPF_PROG("tracepoint/sched/sched_switch", AID_ROOT, AID_SYSTEM, tp_sched_switch)
+(struct switch_args* args) {
     uint32_t zero = 0;
     uint64_t* last = bpf_cpu_last_update_map_lookup_elem(&zero);
     if (!last) return 0;
@@ -134,8 +134,8 @@ struct cpufreq_args {
     unsigned int cpu_id;
 };
 
-SEC("tracepoint/power/cpu_frequency")
-int tp_cpufreq(struct cpufreq_args* args) {
+DEFINE_BPF_PROG("tracepoint/power/cpu_frequency", AID_ROOT, AID_SYSTEM, tp_cpufreq)
+(struct cpufreq_args* args) {
     uint32_t cpu = args->cpu_id;
     unsigned int new = args->state;
     uint32_t* policyp = bpf_cpu_policy_map_lookup_elem(&cpu);
